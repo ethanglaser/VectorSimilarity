bd44ba8a1e34ce0c277335bd6dfd31bff4b91b83
lerman25 <58445352+lerman25@users.noreply.github.com>
2025-03-10 10:42:46 +0200

    fix dim

--- tests/benchmark/run_files/bm_basics_multi_int8.cpp
+++ tests/benchmark/run_files/bm_basics_multi_int8.cpp
@@ -10,7 +10,7 @@ bool BM_VecSimGeneral::is_multi = true;
 
 size_t BM_VecSimGeneral::n_queries = 10000;
 size_t BM_VecSimGeneral::n_vectors = 1000000;
-size_t BM_VecSimGeneral::dim = 512;
+size_t BM_VecSimGeneral::dim = 1024;
 size_t BM_VecSimGeneral::M = 64;
 size_t BM_VecSimGeneral::EF_C = 512;
 tieredIndexMock BM_VecSimGeneral::mock_thread_pool{};
